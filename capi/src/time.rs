1c70800f2099b3a9e5e81d1d0093a23866bf2474
Callie LeFave <antymew@gmail.com>
2018-03-20 21:43:57 -0400

    Extend Time C API - Expose `new`, `zero` - Implement `set_real_time`, `set_game_time`, `set_index` - Expose `add`, `sub`, `add_assign`, `sub_assign`

--- capi/src/time.rs
+++ capi/src/time.rs
@@ -1,13 +1,26 @@
 //! A time that can store a Real Time and a Game Time. Both of them are
 //! optional.
 
-use livesplit_core::{Time, TimingMethod};
-use super::{acc, alloc, own_drop};
+use livesplit_core::{Time, TimingMethod, TimeSpan};
+use super::{acc, acc_mut, alloc, own_drop};
 use std::ptr;
-use time_span::NullableTimeSpan;
+use time_span::{NullableTimeSpan};
 
 /// type
 pub type OwnedTime = *mut Time;
+
+/// Creates a new Time with empty Real Time and Game Time.
+#[no_mangle]
+pub unsafe extern "C" fn Time_new() -> OwnedTime {
+    alloc(Time::new())
+}
+
+/// Creates a new Time where Real Time and Game Time are zero. Keep in mind
+/// that a zero Time Span is not the same as an empty Time Span.
+#[no_mangle]
+pub unsafe extern "C" fn Time_zero() -> OwnedTime {
+    alloc(Time::zero())
+}
 
 /// Clones the time.
 #[no_mangle]
@@ -41,6 +54,26 @@ pub unsafe extern "C" fn Time_game_time(this: *const Time) -> *const NullableTim
         .unwrap_or_else(ptr::null)
 }
 
+/// Set the Real Time value.
+#[no_mangle]
+pub unsafe extern "C" fn Time_set_real_time(this: *mut Time, time: *const NullableTimeSpan) {
+    acc_mut(this).real_time = if time == ptr::null() {
+        None
+    } else {
+        Some(acc(time).clone())
+    };
+}
+
+/// Set the Game Time value.
+#[no_mangle]
+pub unsafe extern "C" fn Time_set_game_time(this: *mut Time, time: *const NullableTimeSpan) {
+    acc_mut(this).game_time = if time == ptr::null() {
+        None
+    } else {
+        Some(acc(time).clone())
+    };
+}
+
 /// Access the time's value for the timing method specified.
 #[no_mangle]
 pub unsafe extern "C" fn Time_index(
@@ -51,4 +84,43 @@ pub unsafe extern "C" fn Time_index(
         .as_ref()
         .map(|t| t as *const _)
         .unwrap_or_else(ptr::null)
+}
+
+/// Set the time's value for the timing method specified.
+#[no_mangle]
+pub unsafe extern "C" fn Time_set_index(
+    this: *mut Time,
+    timing_method: TimingMethod,
+    time: *const NullableTimeSpan,
+) {
+    acc_mut(this)[timing_method] = if time == ptr::null() {
+        None
+    } else {
+        Some(acc(time).clone())
+    };
+}
+
+/// Create a new time from the result of adding this time to another time.
+#[no_mangle]
+pub unsafe extern "C" fn Time_add(this: *const Time, rhs: *const Time) -> OwnedTime {
+    alloc(*acc(this) + *acc(rhs))
+}
+
+/// Create a new time from the result of subtracting another time from this
+/// time.
+#[no_mangle]
+pub unsafe extern "C" fn Time_sub(this: *const Time, rhs: *const Time) -> OwnedTime {
+    alloc(*acc(this) - *acc(rhs))
+}
+
+/// Add another time to this time.
+#[no_mangle]
+pub unsafe extern "C" fn Time_add_assign(this: *mut Time, rhs: *const Time) {
+    *acc_mut(this) += *acc(rhs);
+}
+
+/// Subtracts another time from this time.
+#[no_mangle]
+pub unsafe extern "C" fn Time_sub_assign(this: *mut Time, rhs: *const Time) {
+    *acc_mut(this) -= *acc(rhs);
 }
